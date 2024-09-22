0e2bbdb658be60326f2f2cc643a6f055b3b716a8
danielin0526 <danielarpe05@gmail.com>
2024-09-22 17:41:55 +0200

    Ejercicios 00 Terminados

--- 00 Processos/EjerciciosArtiagaD/ejercicio02/src/main/java/com/project/Main.java
+++ 00 Processos/EjerciciosArtiagaD/ejercicio02/src/main/java/com/project/Main.java
@@ -1,7 +1,25 @@
 package com.project;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CompletableFuture<Integer> validacio = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Validant les dades de la solicitud");
+            return 150;
+        });
+
+        CompletableFuture<Integer> modificacio = validacio.thenApply(result -> {
+            System.out.println("Modificant les dades");
+            return result / 10;
+        });
+
+        try {
+            Integer finalResult = modificacio.get();
+            System.out.println("Resultat final = " + finalResult);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
